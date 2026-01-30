6c64b2d14381c949abdcb4bae77a013ec53d064c
André Pettersson <andre20030417@gmail.com>
2026-01-30 15:21:23 +0100

    Feat: Add InitiateSwap command and controller

---
+++ ShiftMate/ShiftMate.Api/Controllers/SwapRequestsController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ShiftMate.Application.SwapRequests.Commands;
+
+namespace ShiftMate.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwapRequestsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SwapRequestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // POST: api/SwapRequests/initiate
+        [HttpPost("initiate")]
+        public async Task<IActionResult> InitiateSwap(InitiateSwapCommand command)
+        {
+            try
+            {
+                var swapRequestId = await _mediator.Send(command);
+                return Ok(new { SwapRequestId = swapRequestId, Message = "Bytesförfrågan skapad!" });
+            }
+            catch (Exception ex)
+            {
+                // Om något gick fel (t.ex. fel användare), returnera 400 Bad Request
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
